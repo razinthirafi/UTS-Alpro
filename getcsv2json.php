5676ddea5323a90f53e12f8d3d9a7d322d0cee8f
Razin Thirafi <71157540+razinthirafi@users.noreply.github.com>
2023-10-26 10:39:50 +0700

    Add files via upload

---
+++ getcsv2json.php
@@ -0,0 +1,39 @@
+<?php
+
+function csvToJson($csvUrl) {
+    $csvData = [];
+
+    if (($handle = fopen($csvUrl, 'r')) !== false) {
+        while (($row = fgetcsv($handle)) !== false) {
+            $csvData[] = $row;
+        }
+        fclose($handle);
+    }
+
+    // Assuming the first row of the CSV contains the column headers
+    $headers = array_shift($csvData);
+
+    $jsonArray = [];
+
+    foreach ($csvData as $row) {
+        $jsonArrayItem = [];
+
+        foreach ($row as $i => $value) {
+            $jsonArrayItem[$headers[$i]] = $value;
+        }
+
+        $jsonArray[] = $jsonArrayItem;
+    }
+
+    return json_encode($jsonArray);
+}
+
+$csvUrl = 'https://testingalpro.alwaysdata.net/api/coffee.csv';
+$jsonData = csvToJson($csvUrl);
+
+// Set the content type to JSON
+header('Content-Type: application/json');
+
+// Output the JSON data
+echo $jsonData;
+?>
